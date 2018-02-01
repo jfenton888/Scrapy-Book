610d2758034e0fb5da4d9c9cff72325f6d53d334
Jack Fenton <mllax8@me.com>
2018-02-01 11:00:13 -0500

    Final

--- brickset-scraper/scrappy_demo.py
+++ brickset-scraper/scrappy_demo.py
@@ -66,34 +66,34 @@ import scrapy
 
 
 
-class BrickSetSpider(scrapy.Spider):
-    name = "demo_spider"
-    start_urls = ['https://brickset.com/sets/']
+# class BrickSetSpider(scrapy.Spider):
+#     name = "demo_spider"
+#     start_urls = ['https://brickset.com/sets/']
 
-    def parse(self, response):
-        SET_SELECTOR = '.set'
-        for brickset in response.css(SET_SELECTOR):
+#     def parse(self, response):
+#         SET_SELECTOR = '.set'
+#         for brickset in response.css(SET_SELECTOR):
 
-            NAME_SELECTOR = 'h1 a ::text'
-            PIECES_SELECTOR = 'a.//dl[dt/text() = "Pieces"]/dd/a/text()'
-            MINIFIGS_SELECTOR = 'a.//dl[dt/text() = "Minifigs"]/dd[2]/a/text()'
-            IMAGE_SELECTOR = 'img ::attr(src)'
-            SET_PRICE = 'a.//dl[dt/text() = "RRP"]/text()'
-            yield {
-                'number': (brickset.css(NAME_SELECTOR).extract())[0],
-                'name': (brickset.css(NAME_SELECTOR).extract())[0],
-                'pieces': brickset.xpath(PIECES_SELECTOR).extract_first(),
-                'minifigs': brickset.xpath(MINIFIGS_SELECTOR).extract_first(),
-                'price' : brickset.xpath(SET_PRICE).extract_first(),
-                'image': brickset.css(IMAGE_SELECTOR).extract_first(),
-            }
-        NEXT_PAGE_SELECTOR = '.next a ::attr(href)'
-        next_page = response.css(NEXT_PAGE_SELECTOR).extract_first()
-        if next_page:
-            yield scrapy.Request(
-                response.urljoin(next_page),
-                callback=self.parse
-            )
+#             NAME_SELECTOR = 'h1 a ::text'
+#             PIECES_SELECTOR = 'a.//dl[dt/text() = "Pieces"]/dd/a/text()'
+#             MINIFIGS_SELECTOR = 'a.//dl[dt/text() = "Minifigs"]/dd[2]/a/text()'
+#             IMAGE_SELECTOR = 'img ::attr(src)'
+#             SET_PRICE = 'a.//dl[dt/text() = "RRP"]/text()'
+#             yield {
+#                 'number': (brickset.css(NAME_SELECTOR).extract())[0],
+#                 'name': (brickset.css(NAME_SELECTOR).extract())[0],
+#                 'pieces': brickset.xpath(PIECES_SELECTOR).extract_first(),
+#                 'minifigs': brickset.xpath(MINIFIGS_SELECTOR).extract_first(),
+#                 'price' : brickset.xpath(SET_PRICE).extract_first(),
+#                 'image': brickset.css(IMAGE_SELECTOR).extract_first(),
+#             }
+#         NEXT_PAGE_SELECTOR = '.next a ::attr(href)'
+#         next_page = response.css(NEXT_PAGE_SELECTOR).extract_first()
+#         if next_page:
+#             yield scrapy.Request(
+#                 response.urljoin(next_page),
+#                 callback=self.parse
+#             )
 
 
 
